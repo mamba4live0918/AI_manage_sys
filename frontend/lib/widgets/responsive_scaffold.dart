dc4291076b8a86df9092596d581bf6772ad70091
Yinbo zhao <yzha2354@uni.sydney.edu.au>
2026-05-31 20:41:21 +0800

    fix: move mobile burger to right side

--- frontend/lib/widgets/responsive_scaffold.dart
+++ frontend/lib/widgets/responsive_scaffold.dart
@@ -175,8 +175,8 @@ class _ResponsiveScaffoldState extends ConsumerState<ResponsiveScaffold> {
               // thin transparent toolbar so burger doesn't overlap content
               Container(
                 height: topPadding + 44,
-                padding: EdgeInsets.only(top: topPadding + 4, left: 12),
-                alignment: Alignment.centerLeft,
+                padding: EdgeInsets.only(top: topPadding + 4, right: 12),
+                alignment: Alignment.centerRight,
                 child: _MobileBurger(
                   isDark: isDark,
                   onTap: () => setState(() => _mobileDrawerOpen = true),
